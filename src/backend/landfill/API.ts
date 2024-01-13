4d8ee710d3bfa1ce6dab168c61937de644d7a5c2
iiTONELOC <anthonytropeano@me.com>
2024-01-12 19:27:52 -0500

    Adjusted The Auth Token renewal

--- src/backend/landfill/API.ts
+++ src/backend/landfill/API.ts
@@ -1,14 +1,15 @@
 import { getEncryptionKey, getLoggedInUser } from '../../ipcControllers';
 import { decrypt } from '../utils/_crypto';
 
+let authToken = '';
+let lastRefreshed = 0;
 const fiftyMinutes = 50 * 60 * 1000;
+let timeOut: NodeJS.Timeout | null = null;
+let authTokenExpiresIn: number = fiftyMinutes;
+const upcServerURL = (): string => process.env.NODE_ENV === 'production' ?
+    'https://the-landfill.herokuapp.com/graphql' : 'http://localhost:3001/graphql';
 
 class LandFillAPI {
-    private authToken = '';
-    private authTokenExpiresIn: number = fiftyMinutes;
-    private readonly upcServerURL: string = process.env.NODE_ENV === 'production' ?
-        'https://the-landfill.herokuapp.com/graphql' : 'http://localhost:3001/graphql';
-
 
     async logInToUPCServer(): Promise<boolean> {
         const currentUser = getLoggedInUser();
@@ -29,7 +30,7 @@ class LandFillAPI {
             }
         };
 
-        const loginResponse = await fetch(this.upcServerURL, {
+        const loginResponse = await fetch(upcServerURL(), {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(loginMutation)
@@ -38,18 +39,23 @@ class LandFillAPI {
         const loginResponseJSON = await loginResponse.json();
         const { data } = loginResponseJSON;
 
+        // // log any errors
+        // if (loginResponseJSON.errors) {
+        //     console.log('LOGIN ERROR:', loginResponseJSON.errors);
+        // }
 
         const token = data?.loginUserDevice?.token;
 
         if (token) {
-            this.authToken = token;
-
+            authToken = token;
+            lastRefreshed = Date.now();
             // set a timer to refresh the token
+            timeOut && clearTimeout(timeOut);
 
-            setTimeout(() => {
-                this.authTokenExpiresIn = fiftyMinutes;
-                this.logInToUPCServer();
-            }, this.authTokenExpiresIn - 1000);
+            timeOut = setTimeout(async () => {
+                authTokenExpiresIn = fiftyMinutes;
+                await this.logInToUPCServer();
+            }, authTokenExpiresIn - 1000);
             return true;
         }
 
@@ -57,6 +63,17 @@ class LandFillAPI {
     }
 
     async addItemToUsersDefaultList(barcode: string) {
+
+        // ensure that we are logged in, if something interrupts the timeOut like a screen lock or something
+        // we will need to ensure the token isn't stale
+
+        // check if the last refreshed time is greater than the token expiration time
+        // if it is then we need to log in again
+        if (lastRefreshed + authTokenExpiresIn < Date.now() || !authToken || authToken === '') {
+            // console.log('logging in again');
+            await this.logInToUPCServer();
+        }
+
         const addItemMutation = {
             operationName: 'addItemToDefaultList',
             query: `mutation addItemToDefaultList($barcode: String!) {addItemToDefaultList(barcode: $barcode)
@@ -64,13 +81,24 @@ class LandFillAPI {
             variables: { barcode }
         };
 
-        const addItemResponse = await fetch(this.upcServerURL, {
+        // console.log({ addItemMutation, authToken });
+
+        const addItemResponse = await fetch(upcServerURL(), {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json', 'Authorization': `${this.authToken}` },
+            headers: { 'Content-Type': 'application/json', 'Authorization': `${authToken}` },
             body: JSON.stringify(addItemMutation)
         });
 
+        // console.log({ addItemResponse });
+
         const addItemResponseJSON = await addItemResponse.json();
+
+        // console.log({ addItemResponseJSON });
+        // // log any errors
+        // if (addItemResponseJSON.errors) {
+        //     console.log(addItemResponseJSON.errors);
+        // }
+        // console.log()
 
         const { data } = addItemResponseJSON;
         const { addItemToDefaultList } = data;
